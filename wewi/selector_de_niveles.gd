91a4328274514267e461aeb49f3962a3aa2edb4b
thiago-morales <thiagoemanuelmorales@gmail.com>
2025-09-22 12:45:10 -0300

    22/09 selector de niveles tertminado

--- wewi/selector_de_niveles.gd
+++ wewi/selector_de_niveles.gd
@@ -5,5 +5,6 @@ func _on_back_pressed() -> void:
 	get_tree().change_scene_to_file("res://Escenas/menu y selector/menu.tscn")
 
 
-func _ready():
-	for i in 
+
+func _on_nivel_1_pressed() -> void:
+	get_tree().change_scene_to_file("res://Escenas/Mundo4.tscn")
