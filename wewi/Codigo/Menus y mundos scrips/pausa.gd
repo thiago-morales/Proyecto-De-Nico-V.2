6dda74d5d4a28fba23c1415046b61a12ac0d3fc8
thiago-morales <thiagoemanuelmorales@gmail.com>
2025-10-30 11:25:25 -0300

    30/10 cositas para hacerle

---
+++ wewi/Codigo/Menus y mundos scrips/pausa.gd
@@ -0,0 +1,7 @@
+extends CanvasLayer
+
+func _physics_process(delta):
+	if Input.is_action_just_pressed("pausa"):
+		get_tree().paused = not get_tree().paused
+		$ColorRect.visible = not $ColorRect.visible
+		$Label.visible = not $Label.visible
