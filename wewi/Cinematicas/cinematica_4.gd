96033f3d231519d321a81a7c43f6f4004ac6e3df
thiago-morales <thiagoemanuelmorales@gmail.com>
2025-11-04 20:59:19 -0300

    4/11 hice varias cosas en taller

--- wewi/Cinematicas/cinematica_4.gd
+++ wewi/Cinematicas/cinematica_4.gd
@@ -90,4 +90,4 @@ func _process(delta):
 						tween_light_on.tween_property(next_light, "energy", 1.0, 0.3).set_delay(0.3)
 				else:
 					# Si ya no hay más imágenes, pasar de escena
-					get_tree().change_scene_to_file("res://Escenas/menu y selector/menu.tscn")
+					get_tree().change_scene_to_file("res://Escenas/menu y selector/despedida.tscn")
--- wewi/Codigo/Menus y mundos scrips/Caida.gd
+++ wewi/Codigo/Menus y mundos scrips/Caida.gd
@@ -1,5 +1,6 @@
 extends Area2D
 
+@onready var jugador: CharacterBody2D = $"../CharacterBody2D"
 
 # Called when the node enters the scene tree for the first time.
 
@@ -7,4 +8,8 @@ extends Area2D
 func _on_body_entered(body: Node2D) -> void:
 	print("caiste")
 
+	if jugador and jugador.has_method("morir"):
+		jugador.morir()
+
+	await get_tree().create_timer(1.5).timeout
 	get_tree().reload_current_scene()
